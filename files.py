9e7ad79483e9c47cefcb04e34423c2417d3d581f
TJAY-DEV0 <tijaniusman708@gmail.com>
2025-08-22 19:30:22 +0100

    Update files.py

--- files.py
+++ files.py
@@ -4,10 +4,9 @@ import json
 class MyFile:
     def __init__(self,path):
         self.path = path
-        self.full_app_path = os.path.join(self.path,"App")
-    
+        
     def create_files(self):
-        os.makedirs(self.full_app_path,exist_ok=True)
+        os.makedirs(self.path,exist_ok=True)
         
         files_to_create = {
         'history.json':'[]',
@@ -15,7 +14,7 @@ class MyFile:
         'button.json':json.dumps({'color':'blue'})}
             
         for file_name,default_contents in files_to_create.items():
-                file_path = os.path.join(self.full_app_path,file_name)
+                file_path = os.path.join(self.path,file_name)
                 
                 if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
                     with open(file_path,"wt") as f:
@@ -24,11 +23,11 @@ class MyFile:
                 
 
     def read_file(self,file_name):
-        with open(os.path.join(self.full_app_path,file_name),'rt') as f:
+        with open(os.path.join(self.path,file_name),'rt') as f:
             return json.loads(f.read())
                 
     def write_file(self,file_name,content):
-        with open(os.path.join(self.full_app_path,file_name),'wt') as f:
+        with open(os.path.join(self.path,file_name),'wt') as f:
             f.write(json.dumps(content))
                     
                 
